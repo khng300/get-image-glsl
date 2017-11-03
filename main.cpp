a907fbcde1e940c4f16cc946208b73709f2efb17
Andrei Lascu <andrei.lascu10@imperial.ac.uk>
2017-11-03 13:10:16 +0000

    Updated handling of index buffers.

--- main.cpp
+++ main.cpp
@@ -561,43 +561,20 @@ void openglInit(const Params& params, const std::string& fragContents) {
     }
     GLuint vertPosLoc = (GLuint) vertPosLocInt;
 
-	const float vertices[] = {
-		-1.0f,  1.0f,
-		-1.0f, -1.0f,
-		1.0f, -1.0f,
-		1.0f,  1.0f
-	};
-
-	const GLubyte indices[] = {
-		0, 1, 2,
-		2, 3, 0
-	};
-
-    // Vertex buffer array handling changes with OpenGL version, see
-    // https://stackoverflow.com/a/21652955
+    const float vertices[] = {
+        -1.0f, -1.0f, -1.0f, 1.0f, 1.0f, -1.0f,
+         1.0f,  1.0f, -1.0f, 1.0f, 1.0f, -1.0f
+    };
 
     GLuint vertexBuffer;
+    GLuint vertexArray;
+    GL_SAFECALL(glGenVertexArrays, 1, &vertexArray);
+    GL_SAFECALL(glBindVertexArray, vertexArray);
     GL_SAFECALL(glGenBuffers, 1, &vertexBuffer);
     GL_SAFECALL(glBindBuffer, GL_ARRAY_BUFFER, vertexBuffer);
     GL_SAFECALL(glBufferData, GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-    GLuint vertexArray;
-	if (params.API == API_OPENGL) {
-		GL_SAFECALL(glGenVertexArrays, 1, &vertexArray);
-		GL_SAFECALL(glBindVertexArray, vertexArray);
-		GL_SAFECALL(glVertexAttribPointer, vertPosLoc, 2, GL_FLOAT, GL_FALSE, 0, 0);
-		GL_SAFECALL(glEnableVertexAttribArray, vertPosLoc);
-	}
-
-    GLuint indicesBuffer;
-    GL_SAFECALL(glGenBuffers, 1, &indicesBuffer);
-    GL_SAFECALL(glBindBuffer, GL_ELEMENT_ARRAY_BUFFER, indicesBuffer);
-    GL_SAFECALL(glBufferData, GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
     GL_SAFECALL(glEnableVertexAttribArray, vertPosLoc);
-    GL_SAFECALL(glBindBuffer, GL_ARRAY_BUFFER, vertexBuffer);
     GL_SAFECALL(glVertexAttribPointer, vertPosLoc, 2, GL_FLOAT, GL_FALSE, 0, 0);
-    GL_SAFECALL(glBindBuffer, GL_ELEMENT_ARRAY_BUFFER, indicesBuffer);
 
     GL_SAFECALL(glUseProgram, program);
     setUniformsJSON(program, params);
@@ -610,7 +587,9 @@ void openglInit(const Params& params, const std::string& fragContents) {
 void openglRender(const Params& params) {
     GL_SAFECALL(glClearColor, 0.0f, 0.0f, 0.0f, 1.0f);
     GL_SAFECALL(glClear, GL_COLOR_BUFFER_BIT);
-    GL_SAFECALL(glDrawElements, GL_TRIANGLES, 6, GL_UNSIGNED_BYTE, 0);
+//    GL_SAFECALL(glDrawElements, GL_TRIANGLES, 6, GL_UNSIGNED_BYTE, 0);
+    GL_SAFECALL(glDrawArrays, GL_TRIANGLES, 0, 3);
+    GL_SAFECALL(glDrawArrays, GL_TRIANGLES, 3, 3);
     GL_SAFECALL(glFlush);
 }
 
@@ -676,7 +655,6 @@ int main(int argc, char* argv[])
             }
         }
     }
-
     contextTerminate(context);
     exit(EXIT_SUCCESS);
 }
