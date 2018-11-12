39a73ef1936d8c28fecd0d4c07fccd4766cd1c4b
Paul Thomson <paulthomson@google.com>
2018-11-12 16:42:34 +0000

    Try updating gcc also.

--- buildscripts/1-install-deps-travis.sh
+++ buildscripts/1-install-deps-travis.sh
@@ -28,8 +28,9 @@ then
   # The JSON library requires GCC >= 4.9
   sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y;
   sudo apt-get update -qq
-  sudo apt-get install -qq -y g++-4.9
+  sudo apt-get install -qq -y g++-4.9 gcc-4.9
   sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.9 90
+  sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.9 90
 
   sudo apt-get -y install libxrandr-dev libxinerama-dev libxcursor-dev libgl1-mesa-dev cmake zip git
 
